506f5bb3b1efa1d5f74e2281a7e2a770291986fb
Arianne Pablo <arianne_escoto@yahoo.com>
2021-01-21 13:06:38 -0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Arianne Pablo on 1/21/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
