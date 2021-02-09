907c6ba6f585094fa51cab34026b5aca38fe3e2a
Arianne Pablo <arianne_escoto@yahoo.com>
2021-02-09 07:31:34 -0800

    Changed the VStacks to Sections and created a wheel picker for the amount of coffee

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,11 +8,119 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter a time",
+                               selection: $wakeUp,
+                               displayedComponents:
+                                .hourAndMinute)
+                        .labelsHidden()
+                            .datePickerStyle(WheelDatePickerStyle())
+                }
+                
+                Section {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper(value: $sleepAmount, in:
+                        4...12, step: 0.25) {
+                            Text("\(sleepAmount, specifier: "%g") hours")
+                    }
+                }
+                
+                Section {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Picker("", selection: $coffeeAmount) {
+                        ForEach(1...20, id: \.self) { cups in
+                            if (cups == 1) {
+                                Text("1 cup")
+                            } else {
+                                Text("\(cups) cups")
+                            }
+                        }
+                    }
+                    .pickerStyle(WheelPickerStyle())
+                    
+/*                    Stepper(value: $coffeeAmount, in:
+                        1...20) {
+                        if coffeeAmount == 1 {
+                            Text("1 cup")
+                        }
+                        else {
+                            Text("\(coffeeAmount) cups")
+                        }
+                    }
+*/                } // end coffee vstack
+            } // end Form
+            
+            .navigationBarTitle("BetterRest")
+            .navigationBarItems(trailing:
+                Button(action: calculateBedtime) {
+                    Text("Calculate")
+                })
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text(alertTitle),
+                      message: Text(alertMessage),
+                      dismissButton: .default(Text("OK")))
+            }
+        } // end NavigationView
+    } // end body
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
     }
-}
+    
+    
+    func calculateBedtime() {
+        let model = SleepCalculator()
+        
+        let components =
+            Calendar.current.dateComponents([.hour, .minute],
+                from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        
+        do {
+            let prediction = try
+                model.prediction(wake: Double(hour + minute),
+                                estimatedSleep: sleepAmount,
+                                coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            
+            alertMessage = formatter.string(from: sleepTime)
+            alertTitle = "Your ideal bedtime is..."
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            
+        }
+        
+        showingAlert = true
+    }
+    
+} // end View
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
